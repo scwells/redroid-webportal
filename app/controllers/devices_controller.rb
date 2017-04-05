97d9e36897a456c68e39e34f01e7cca7d7837496
Ubuntu <redroid@ubuntu-server.cbkaotjzwfou3axxpn03bzsmja.dx.internal.cloudapp.net>
2017-04-05 16:51:01 -0600

    various UI improvements; video window now scrolls with the window on the video save screen for a device

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -138,8 +138,9 @@ class DevicesController < ApplicationController
     def get_device_directory(device_id)
       if Dir.exist?(file_path = "/home/redroid/video_saves/#{device_id}")
         files = Dir.entries(file_path).select { |e| /\A(\w|\d)+[_]([-:\w\d])+(.flv)\z/ =~ e  }
+        files = files.sort_by { |file| File.mtime(file_path + '/' + file) }
         result = []
-        files.each do |file|
+        files.reverse_each do |file|
           result << { name: file, 
                       size: '%.2f' % (File.size("#{file_path}/#{file}").to_f / 2**20 ) }
         end
